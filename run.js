296d0d01cd32128f7cf4a93cc2ea4697dac1383f
Jamb <jambdoe@gmail.com>
2021-06-10 08:17:20 -0400

    improved typescript path locating

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
 const path = require('path');
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
-
-script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
+let relative = __dirname.lastIndexOf(path.sep + "node_modules" + path.sep);
+script(path.resolve(relative === -1 ? '' : __dirname.slice(0, relative), 'node_modules/typescript/bin/tsc'));
